02b6cafc1fff7578a77142bc79e810789178d3a6
Ophelia-A <opheliaayisi10@gmail.com>
2024-05-18 18:21:56 -0200

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -17,3 +17,21 @@ function processArray(values){
 let values = [1,2,3,4,5,6,7,8,9,10,11,12];
 let newValues = processArray(values);
 console.log(newValues);
+
+
+function formatArrayStrings(strings, result){
+  let formattedStrings =[];
+for (let n = 0; n < strings.length; n++) {
+  if(result[n] % 2 === 0) {
+    formattedStrings.push(strings[n].toUpperCase());
+     }
+  else {    formattedStrings.push(strings[n].toLowerCase());
+};
+};
+return formattedStrings;
+};     
+
+let strings = ["Ophelia","Roselyn", "Kwame", "Adiza","Geraldina"]
+let formattedStrings = formatArrayStrings(strings, newValues);
+console.log(formattedStrings);
+
